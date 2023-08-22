099b681c8207410a10c9b686c8f97c6e8cd3e250
Lasha Nikolaishvili <nikolaishvililashan1@gmail.com>
2023-08-23 00:58:36 +0400

    adding question pagination and styles

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Question({questionData}) {
+export default function Question({questionData, setQuestionNum, setUserAnswers}) {
     const {correct_answer: correctAnswer, incorrect_answers: incorrectAnswers, question} = questionData
     
     function convertHtmlToText(html) {
@@ -13,16 +13,23 @@ export default function Question({questionData}) {
         return array.slice().sort(() => Math.random() - 0.5);
     }
 
+    function handleClick(answerIndex) {
+        const userAnswer = allAnswers[answerIndex]
+        setUserAnswers((prevUserAnsweres => [...prevUserAnsweres, {userAnswer, correctAnswer}]))
+
+        setQuestionNum(prevNum => prevNum + 1)
+    }
+
     const allAnswers = shuffleArray([...incorrectAnswers, correctAnswer])
    
     return (
         <div>
-            <p className='question'>{convertHtmlToText(question)}</p>
+            <h1 className='question'>{convertHtmlToText(question)}</h1>
             <div className="btn-group">
-                <button className='btn-group__btn'>{convertHtmlToText(allAnswers[0])}</button>
-                <button className='btn-group__btn'>{convertHtmlToText(allAnswers[1])}</button>
-                <button className='btn-group__btn'>{convertHtmlToText(allAnswers[2])}</button>
-                <button className='btn-group__btn'>{convertHtmlToText(allAnswers[3])}</button>
+                <button onClick={() => handleClick(0)} className='btn-group__btn'>{convertHtmlToText(allAnswers[0])}</button>
+                <button onClick={() => handleClick(1)} className='btn-group__btn'>{convertHtmlToText(allAnswers[1])}</button>
+                <button onClick={() => handleClick(2)} className='btn-group__btn'>{convertHtmlToText(allAnswers[2])}</button>
+                <button onClick={() => handleClick(3)} className='btn-group__btn'>{convertHtmlToText(allAnswers[3])}</button>
             </div>
         </div>
     )
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,17 +2,16 @@ import React from 'react'
 import Question from './Question'
 
 export default function Quiz({setHasStarted}) {
-    // const [questionNum, setQuestionNum] = React.useState(0);
+    const [questionNum, setQuestionNum] = React.useState(0);
     const [quizData, setQuizData] = React.useState([]);
+    const [userAnswers, setUserAnswers] = React.useState([]);
 
     React.useEffect(() => {
         async function fetchQuizData() {
             try {
                 const res = await fetch('https://opentdb.com/api.php?amount=6&type=multiple');
                 const json = await res.json()
-                console.log(json.results)
                 setQuizData(json.results)
-                console.log("quiz data: ", quizData)
             } catch (error) {
                 console.log(error)
             }
@@ -20,18 +19,30 @@ export default function Quiz({setHasStarted}) {
 
         fetchQuizData();
     }, [])
-    
-    React.useEffect(() => {
-        console.log("quiz data: ", quizData); 
-    }, [quizData]);
+
+    function handleQuizEnd(questionNum) {
+        let correctCnt = 0
+
+        if(questionNum > 4) {
+            correctCnt = userAnswers.reduce((cnt, currAnswer) => {
+                if(currAnswer.userAnswer === currAnswer.correctAnswer) cnt++;
+            }, 0)
+            console.log(correctCnt)    
+        }
+
+        return <h1>You got {correctCnt}/5 </h1>
+    }
 
     return (
         <div className='quiz-window'>
-            <Question questionData={quizData[0]}/>
-            <Question questionData={quizData[1]}/>
-            <Question questionData={quizData[2]}/>
-            <Question questionData={quizData[3]}/>
-            <Question questionData={quizData[4]}/>
+            {quizData?.[0] && questionNum === 0 && <Question questionData = {quizData[0]} setQuestionNum = {setQuestionNum} setUserAnswers = {setUserAnswers}/>}
+            {quizData?.[1] && questionNum === 1 && <Question questionData = {quizData[1]} setQuestionNum = {setQuestionNum} setUserAnswers = {setUserAnswers}/>}
+            {quizData?.[2] && questionNum === 2 && <Question questionData = {quizData[2]} setQuestionNum = {setQuestionNum} setUserAnswers = {setUserAnswers}/>}
+            {quizData?.[3] && questionNum === 3 && <Question questionData = {quizData[3]} setQuestionNum = {setQuestionNum} setUserAnswers = {setUserAnswers}/>}
+            {quizData?.[4] && questionNum === 4 && <Question questionData = {quizData[4]} setQuestionNum = {setQuestionNum} setUserAnswers = {setUserAnswers}/>}
+            {questionNum <= 4 && <span className='question-num'>{questionNum + 1}/5</span>}
+            {/* {questionNum > 4 && <h1>You got {correctAnswers} </h1>} */}
+            {questionNum > 4 && handleQuizEnd(questionNum)}
         </div>
     )
 }
