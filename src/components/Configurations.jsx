77adc088b21527dc2993fc9654c6f7741d6f08dd
Lasha Nikolaishvili <nikolaishvililashan1@gmail.com>
2023-08-30 20:14:51 +0400

    styling configuration component

--- src/components/Configurations.jsx
+++ src/components/Configurations.jsx
@@ -27,59 +27,68 @@ export default function Configuration({openConfig, handleConfig, setApiQueryStri
 
     return (
         <div className={openConfig ? "configurations active" : "configurations"} >
-            <input 
-                className="configurations__count" 
-                type="number" 
-                name="amount" 
-                min="5" 
-                max="50" 
-                placeholder="Enter a number [5-50]"
-                onChange={handleChange}
-                value={formData.amount}
-            />
-            <select 
-                className="configurations__categories" 
-                name="category"
-                onChange={handleChange}
-                value={formData.category}
-            >
-                <option value="">Any Category</option>
-                <option value="9">General Knowledge</option>
-                <option value="10">Entertainment: Books</option>
-                <option value="11">Entertainment: Film</option>
-                <option value="12">Entertainment: Music</option>
-                <option value="13">Entertainment: Musicals  Theatres</option>
-                <option value="14">Entertainment: Television</option>
-                <option value="15">Entertainment: Video Games</option>
-                <option value="16">Entertainment: Board Games</option>
-                <option value="17">Science &amp; Nature</option>
-                <option value="18">Science: Computers</option>
-                <option value="19">Science: Mathematics</option>
-                <option value="20">Mythology</option>
-                <option value="21">Sports</option>
-                <option value="22">Geography</option>
-                <option value="23">History</option>
-                <option value="24">Politics</option>
-                <option value="25">Art</option>
-                <option value="26">Celebrities</option>
-                <option value="27">Animals</option>
-                <option value="28">Vehicles</option>
-                <option value="29">Entertainment: Comics</option>
-                <option value="30">Science: Gadgets</option>
-                <option value="31">Entertainment: Japanese Anime &amp; Manga</option>
-                <option value="32">Entertainment: Cartoon &amp; Animations</option>
-            </select>
-            <select 
-                className="configurations__difficulty" 
-                name="difficulty"
-                onChange={handleChange}
-                value={formData.difficulty}
-            >
-                <option value="">Any Difficulty</option>
-                <option value="easy">Easy</option>
-                <option value="medium">Medium</option>
-                <option value="hard">Hard</option>
-            </select>
+            <div className="configurations__input-group">
+                <label htmlFor="configurationsCount">Number of Questions</label>
+                <input 
+                    id="configurationsCount"
+                    type="number" 
+                    name="amount" 
+                    min="5" 
+                    max="50" 
+                    placeholder="Enter a number [5-50]"
+                    onChange={handleChange}
+                    value={formData.amount}
+                />
+            </div>
+            <div className="configurations__input-group">
+                <label htmlFor="configurationsCategories">Category</label>
+                <select 
+                    id="configurationsCategories"
+                    name="category"
+                    onChange={handleChange}
+                    value={formData.category}
+                >
+                    <option value="">Any Category</option>
+                    <option value="9">General Knowledge</option>
+                    <option value="10">Entertainment: Books</option>
+                    <option value="11">Entertainment: Film</option>
+                    <option value="12">Entertainment: Music</option>
+                    <option value="13">Entertainment: Musicals  Theatres</option>
+                    <option value="14">Entertainment: Television</option>
+                    <option value="15">Entertainment: Video Games</option>
+                    <option value="16">Entertainment: Board Games</option>
+                    <option value="17">Science &amp; Nature</option>
+                    <option value="18">Science: Computers</option>
+                    <option value="19">Science: Mathematics</option>
+                    <option value="20">Mythology</option>
+                    <option value="21">Sports</option>
+                    <option value="22">Geography</option>
+                    <option value="23">History</option>
+                    <option value="24">Politics</option>
+                    <option value="25">Art</option>
+                    <option value="26">Celebrities</option>
+                    <option value="27">Animals</option>
+                    <option value="28">Vehicles</option>
+                    <option value="29">Entertainment: Comics</option>
+                    <option value="30">Science: Gadgets</option>
+                    <option value="31">Entertainment: Japanese Anime &amp; Manga</option>
+                    <option value="32">Entertainment: Cartoon &amp; Animations</option>
+                </select>
+            </div>
+            <div className="configurations__input-group">
+                <label htmlFor="configurationsDifficulty">Difficulty</label>
+                <select 
+                    id="configurationsDifficulty"
+                    name="difficulty"
+                    onChange={handleChange}
+                    value={formData.difficulty}
+                >
+                    <option value="">Any Difficulty</option>
+                    <option value="easy">Easy</option>
+                    <option value="medium">Medium</option>
+                    <option value="hard">Hard</option>
+                </select>
+            </div>
             <button className="configurations__btn" onClick={handleConfig}></button>
         </div>
     )
