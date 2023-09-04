adb0e04ef2d8f1bdb5de9e75324d5a1c9649a18c
Lasha Nikolaishvili <nikolaishvililashan1@gmail.com>
2023-09-05 01:28:17 +0400

    adding quit button

--- src/components/WarningPopup.jsx
+++ src/components/WarningPopup.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function WarningPopup({openWarningPopup, handleWarningPopup, setHasStarted}) {
 
-    function handleButton() {
+    function handleQuit() {
         handleWarningPopup((prev) => !prev)
         setHasStarted((prev) => !prev)
     }
@@ -11,7 +11,8 @@ export default function WarningPopup({openWarningPopup, handleWarningPopup, setH
         <div className={openWarningPopup ? "warning-popup active" : "warning-popup"} >
             <h2 className="warning-popup__header">Beware</h2>
             <p className="warning-popup__text">Going back to the homepage will result in you losing your progress.</p>
-            <button className="warning-popup__btn" onClick={handleButton}>Proceed</button>
+            <button className="warning-popup__proceed-btn" onClick={() => handleWarningPopup((prev) => !prev)}>Proceed</button>
+            <button className="warning-popup__quit-btn" onClick={handleQuit}>Quit</button>
         </div>
     )
 }
